e01f82db46435f9ccca452fb6616224facc35edf
v1rtl <pilll.pl22@gmail.com>
2021-02-05 19:02:21 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,7 +69,7 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
 
