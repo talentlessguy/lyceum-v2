4699e5ae4849062a1624af94940ea7c6d84e0a20
v1rtl <pilll.pl22@gmail.com>
2021-02-07 18:17:39 +0300

    fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,31 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import style from '../styles/footer.module.css'
 import ExternalLink from './ExternalLink'
 
 export default function Footer() {
+  const router = useRouter()
+
   return (
     <footer className={style.footer}>
       <p>Лицей №2</p>
       <p>Московская область, город Протвино, улица Гагарина, д.7</p>
       <p>Copyright © {new Date().getFullYear()}</p>
 
-      <ExternalLink href="https://v1rtl.site">
-        <img
-          src="https://v1rtl.site/emoji.gif"
-          alt="made by v1rtl"
-          width="30px"
-          style={{
-            position: 'absolute',
-            bottom: '3px',
-            right: '3px'
-          }}
-        />
-      </ExternalLink>
+      {router.pathname === '/' && (
+        <ExternalLink href="https://v1rtl.site">
+          <img
+            src="https://v1rtl.site/emoji.gif"
+            alt="made by v1rtl"
+            width="30px"
+            style={{
+              position: 'absolute',
+              bottom: '3px',
+              right: '3px'
+            }}
+          />
+        </ExternalLink>
+      )}
     </footer>
   )
 }
