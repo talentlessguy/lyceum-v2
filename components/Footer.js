95ce8cad84b40b2602177d37aeec243ac3cbf1a8
v1rtl <pilll.pl22@gmail.com>
2021-02-07 22:55:00 +0300

    remove easter egg

--- components/Footer.js
+++ components/Footer.js
@@ -12,20 +12,7 @@ export default function Footer() {
       <p>Московская область, город Протвино, улица Гагарина, д.7</p>
       <p>Copyright © {new Date().getFullYear()}</p>
 
-      {router.pathname === '/' && (
-        <ExternalLink href="https://v1rtl.site">
-          <img
-            src="https://v1rtl.site/emoji.webp"
-            alt="made by v1rtl"
-            width="30px"
-            style={{
-              position: 'absolute',
-              bottom: '3px',
-              right: '3px'
-            }}
-          />
-        </ExternalLink>
-      )}
+      {router.pathname === '/' && <ExternalLink href="https://v1rtl.site">v1rtl</ExternalLink>}
     </footer>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const fonts = [
-  'Golos-Text_Bold',
-  'Golos-Text_Medium',
-  'Golos-Text_Regular',
-  'Golos-UI_Bold',
-  'Golos-UI_Medium',
-  'Golos-UI_Regular'
-]
+const fonts = ['Golos-Text_Bold', 'Golos-Text_Medium', 'Golos-Text_Regular', 'Golos-UI_Bold', 'Golos-UI_Medium']
 
 class MyDocument extends Document {
   render() {
