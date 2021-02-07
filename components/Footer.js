f147095ba45f7987662a22bef5577157dfd0f0d8
v1rtl <pilll.pl22@gmail.com>
2021-02-07 16:56:05 +0300

    add easter egg

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from '../styles/footer.module.css'
+import ExternalLink from './ExternalLink'
 
 export default function Footer() {
   return (
@@ -7,6 +8,19 @@ export default function Footer() {
       <p>Лицей №2</p>
       <p>Московская область, город Протвино, улица Гагарина, д.7</p>
       <p>Copyright © {new Date().getFullYear()}</p>
+
+      <ExternalLink href="https://v1rtl.site">
+        <img
+          src="https://v1rtl.site/emoji.gif"
+          alt="made by v1rtl"
+          width="30px"
+          style={{
+            position: 'absolute',
+            bottom: '3px',
+            right: '3px'
+          }}
+        />
+      </ExternalLink>
     </footer>
   )
 }
