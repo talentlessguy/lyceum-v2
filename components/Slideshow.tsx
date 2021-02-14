cf4af0237c6da72c3f0f8dade2c9032d4d6c0bb7
v1rtl <pilll.pl22@gmail.com>
2021-02-15 00:13:41 +0300

    fix

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -13,7 +13,15 @@ const PrevButton = (props) => (
 
 const NextButton = (props) => (
   <button {...props}>
-    <svg fill="white" className="embla__button__svg" viewBox="137.7 -1 366.6 644" width="12">
+    <svg
+      fill="white"
+      style={{
+        transform: 'rotate(180deg)'
+      }}
+      className="embla__button__svg"
+      viewBox="137.7 -1 366.6 644"
+      width="12"
+    >
       <path d="M428.4 12.5a42.7 42.7 0 0160.4 60.4L241.7 320l247 247a42.7 42.7 0 01-60.3 60.5l-277.2-277a42.3 42.3 0 01-12.5-30.4c0-11.1 4.1-22 12.5-30.4L428.4 12.5z" />
     </svg>
   </button>
